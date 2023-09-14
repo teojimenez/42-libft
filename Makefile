99fb9a00f70994fb107d0ef0b9604b9f79b5928a
root <root@teojimenez>
2023-09-14 21:06:24 +0200

    update Makefile and libft.h

--- Makefile
+++ Makefile
@@ -12,7 +12,9 @@
 
 NAME       = libft.a
 FLAGS      = -Wall -Wextra -Werror
-SRC = *.c
+SRC = \ ft_isalnum.c ft_strlen.c ft_memset.c ft_isprint.c \
+		ft_isdigit.c ft_isascii.c ft_isalpha.c ft_bzero.c \
+		ft_memmove.c ft_strlcpy.c ft_strlcat \
 OBJECTS = $(SRC:.c=.o)
 CC = gcc
 
--- libft.h
+++ libft.h
@@ -25,5 +25,9 @@ int     ft_isdigit(int c);
 int     ft_isascii(int c);
 int     ft_isalpha(int c);
 void    ft_bzero(void *str, size_t n);
+void    *ft_memcpy(void *dest, const void *src, size_t n); //?
+void    *ft_memmove(void *dest, const void *src, size_t n); //?
+size_t  ft_strlcpy(char *dest, const char *src, size_t size);
+size_t  ft_strlcat(char *dst, const char *src, size_t size);
 
 #endif
