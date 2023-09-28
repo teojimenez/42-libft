26df509203430de2260f5816e5d21ed970059bf3
teojimenez <teojimbru@gmail.com>
2023-09-28 22:00:08 +0200

    update 28/09

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,7 +12,25 @@
 
 char *ft_strtrim(char const *s1, char const *set)
 {
+    char *str;
+    int start;
+    int end;
 
+
+    if (!set)
+        return ((char *)s1)
+    else if (!s1)
+        return (0)
+    start = 0;
+    end = ft_strlen((char *)s1);
+    while(s1[start] && ft_strchr(set, s1[start]))
+        start++;
+    while(s1[end] && ft_strrchr(set, s1[end]))
+        end--;
+    str = ft_substr(s1, start ,(end - start) + 1);
+    if (!str)
+        return (0);
+    return (str);
 }
 
 // s1: La string que debe ser recortada.
